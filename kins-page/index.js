8a70be55ce270ce5c8278e6c2593773d307205c6
Yuunan Kin <yuunan.kin@gmail.com>
2020-08-20 22:01:57 +0900

    change to sass

--- kins-page/index.js
+++ kins-page/index.js
@@ -1,136 +1,137 @@
-const MarkdownIt = require('markdown-it')
-var hljs = require('highlight.js')
+const MarkdownIt = require("markdown-it");
+var hljs = require("highlight.js");
 // pull doms
-const headerEl = document.getElementById('header')
-const sidebarEl = document.getElementById('sidebar')
-const contentEl = document.getElementById('content')
-const siteTitleEl = document.getElementById('site-title')
+const headerEl = document.getElementById("header");
+const sidebarEl = document.getElementById("sidebar");
+const contentEl = document.getElementById("content");
+const siteTitleEl = document.getElementById("site-title");
 
-const themeEl = document.getElementById("theme")
-
+const bodyEl = document.getElementById("body");
 
 // consts
-const host = 'http://csc-conference.southeastasia.cloudapp.azure.com:8000'
+const host = "http://csc-conference.southeastasia.cloudapp.azure.com:8000";
 
 // vars
-let memos = []
-let memoEls = []
+let memos = [];
+let memoEls = [];
 
 // init theme
 const checkTheme = () => {
-    const date = new Date()
-    const hour = date.getHours()
-    if (hour > 6 && hour < 18) {
-        themeEl.href = "./kins-page/light.css"
-    } else {
-        themeEl.href = "./kins-page/dark.css"
-    }
-    setTimeout(() => {
-        checkTheme()
-    }, 60000);
-}
-checkTheme()
+  const date = new Date();
+  const hour = date.getHours();
+  if (hour > 6 && hour < 18) {
+    bodyEl.classList.add("light");
+  } else {
+    bodyEl.classList.add("dark");
+  }
+  setTimeout(() => {
+    checkTheme();
+  }, 60000);
+};
+checkTheme();
 
 // get initial memo
-const showFirst = +(window.location.search.replace(/\?id=/, ''))
+const showFirst = +window.location.search.replace(/\?id=/, "");
 
 let md = new MarkdownIt({
-    html: true,
-    linkify: true,
-    typographer: true,
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                return hljs.highlight(lang, str).value;
-            } catch (e) { }
-        }
-
-        return ''; // use external default escaping
+  html: true,
+  linkify: true,
+  typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (e) {}
     }
-})
+
+    return ""; // use external default escaping
+  },
+});
 
 // functions
 // fetch data
 const fetchData = async () => {
-    try {
-        const res = await fetch(`https://cors-anywhere.herokuapp.com/${host}/publicApi`)
-        memos = await res.json()
-        memos = memos.sort((a, b) => new Date(b.update_at) - new Date(a.update_at))
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const res = await fetch(`${host}/publicApi`);
+    memos = await res.json();
+    memos = memos.sort((a, b) => new Date(b.update_at) - new Date(a.update_at));
+  } catch (error) {
+    console.log(error);
+  }
+};
 // init
 const init = async () => {
-    await fetchData()
-    initRenderer()
-    if (memos.length == 0) return
+  await fetchData();
+  initRenderer();
+  if (memos.length == 0) return;
 
-    memos.forEach((memo, index) => {
-        // replace link
-        memo.content = memo.content.replace(/download\?path/g, `${host}/download?path`)
+  memos.forEach((memo, index) => {
+    // replace link
+    memo.content = memo.content.replace(
+      /download\?path/g,
+      `${host}/download?path`
+    );
 
-        // create dom
-        const newEl = document.createElement('div')
-        newEl.className = "title"
-        newEl.id = `title-${memo.id}`
-        newEl.setAttribute('data-index', index)
-        newEl.innerText = memo.title
-        memoEls.push(newEl)
-        sidebarEl.appendChild(newEl)
-        newEl.addEventListener('click', e => {
-            setCurrent(memos[+e.target.getAttribute('data-index')])
-        })
-    })
-    if (showFirst) {
-        let index = 0;
-        memos.forEach((m, i) => {
-            if (m.id == showFirst) {
-                index = i
-            }
-        })
-        setCurrent(memos[index])
-    } else {
-        setCurrent(memos[0])
-    }
-
-}
+    // create dom
+    const newEl = document.createElement("div");
+    newEl.className = "title";
+    newEl.id = `title-${memo.id}`;
+    newEl.setAttribute("data-index", index);
+    newEl.innerText = memo.title;
+    memoEls.push(newEl);
+    sidebarEl.appendChild(newEl);
+    newEl.addEventListener("click", (e) => {
+      setCurrent(memos[+e.target.getAttribute("data-index")]);
+    });
+  });
+  if (showFirst) {
+    let index = 0;
+    memos.forEach((m, i) => {
+      if (m.id == showFirst) {
+        index = i;
+      }
+    });
+    setCurrent(memos[index]);
+  } else {
+    setCurrent(memos[0]);
+  }
+};
 // set current
 const setCurrent = (memo) => {
-    headerEl.innerText = memo.title
-    contentEl.innerHTML = md.render(memo.content)
-    const pre = document.querySelector('.current')
-    if (pre)
-        pre.classList.remove('current')
-    const current = document.getElementById(`title-${memo.id}`)
-    current.classList.add('current')
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    })
-}
+  headerEl.innerText = memo.title;
+  contentEl.innerHTML = md.render(memo.content);
+  const pre = document.querySelector(".current");
+  if (pre) pre.classList.remove("current");
+  const current = document.getElementById(`title-${memo.id}`);
+  current.classList.add("current");
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 // init renderer
 const initRenderer = () => {
-    var defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
-        return self.renderToken(tokens, idx, options);
+  var defaultRender =
+    md.renderer.rules.link_open ||
+    function (tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options);
     };
 
-    md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-        tokens[idx].attrPush(['target', '_blank']);
-        return defaultRender(tokens, idx, options, env, self);
-    };
-}
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    tokens[idx].attrPush(["target", "_blank"]);
+    return defaultRender(tokens, idx, options, env, self);
+  };
+};
 
 // event handlers
-siteTitleEl.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-    })
-})
-
+siteTitleEl.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
 
 // init
-init()
+init();
